e6b260127fbb4bfffb29e426b81cb303b2ae772f
Bob Haugen <bob.haugen@gmail.com>
2017-03-24 10:17:20 -0500

    improve models, commit for review

--- vocab/models.py
+++ vocab/models.py
@@ -5,11 +5,42 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
 
+
+AGENT_SUBCLASS_OPTIONS = (
+    ('person', _('Person')),
+    ('organization', _('Organization')),
+)
+
+
 @python_2_unicode_compatible
 class Agent(models.Model):
+    """
+    vf:Agent: 
+    https://valueflows.gitbooks.io/valueflows/content/introduction/agents.html
+    https://valueflows.gitbooks.io/valueflows/content/specification/generated-spec.html#d4e666
+    """
     name = models.CharField(_('name'), max_length=255)
     url = models.URLField(_('url'), blank=True)
     note = models.TextField(_('note'), blank=True, null=True)
+    agent_subclass =  models.CharField(_('subclass'),
+        max_length=16, choices=AGENT_SUBCLASS_OPTIONS,
+        default='person')
+    
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+        
+@python_2_unicode_compatible        
+class AgentRelationshipRole(models.Model):
+    """
+    vf:AgentRelationshipRole:
+    not yet defined
+    """
+    name = models.CharField(_('name'), max_length=255)
+    inverse_name = models.CharField(_('name'), max_length=255)
     
     class Meta:
         ordering = ('name',)
@@ -17,12 +48,43 @@ class Agent(models.Model):
     def __str__(self):
         return self.name
         
+    
+@python_2_unicode_compatible
+class AgentRelationship(models.Model):
+    """
+    vf:Relationship:
+    https://valueflows.gitbooks.io/valueflows/content/specification/generated-spec.html#d4e840    
+    """
+    subject = models.ForeignKey(Agent,
+        verbose_name=_('subject', related_name="subject_relationships",))
+    object = models.ForeignKey(Agent,
+        verbose_name=_('object', related_name="object_relationships",))
+    relationship = models.ForeignKey(AgentRelationshipRole,
+        verbose_name=_('relationship', related_name="relationships",))
+    context = models.ForeignKey(Agent,
+        blank=True, null=True,
+        verbose_name=_('context', related_name="context_relationships",))
+        
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+    
 
 @python_2_unicode_compatible
 class Process(models.Model):
+    """
+    vf:Process:
+    https://valueflows.gitbooks.io/valueflows/content/introduction/processes.html
+    https://valueflows.gitbooks.io/valueflows/content/specification/generated-spec.html#d4e829
+    """
     name = models.CharField(_('name'), max_length=255)
     url = models.URLField(_('url'), blank=True)
     note = models.TextField(_('note'), blank=True, null=True)
+    planned_start = models.DateTimeField(_('planned start'), blank=True, null=True)
+    planned_duration = models.DurationField(_('planned duration'), blank=True, null=True)
+    is_finished = models.BooleanField(_('is finished'), default=False)
     
     class Meta:
         ordering = ('name',)
@@ -33,6 +95,11 @@ class Process(models.Model):
         
 @python_2_unicode_compatible
 class EconomicResource(models.Model):
+    """
+    vf.EconomicResource:
+    https://valueflows.gitbooks.io/valueflows/content/introduction/resources.html
+    https://valueflows.gitbooks.io/valueflows/content/specification/generated-spec.html#d4e758
+    """
     name = models.CharField(_('name'), max_length=255)
     url = models.URLField(_('url'), blank=True)
     note = models.TextField(_('note'), blank=True, null=True)
@@ -42,12 +109,24 @@ class EconomicResource(models.Model):
 
     def __str__(self):
         return self.name
+
         
+RESOURCE_EFFECT_OPTIONS = (
+    ('increment', _('Increment')),
+    ('decrement', _('Decrement')),
+    ('none', _('None')),
+)
 
 @python_2_unicode_compatible
 class Action(models.Model):
+    """
+    vf:Action:
+    https://valueflows.gitbooks.io/valueflows/content/specification/generated-spec.html#d4e688
+    """
     name = models.CharField(_('name'), max_length=255)
     note = models.TextField(_('note'), blank=True, null=True)
+    resource_effect = models.CharField(_('resource effect'),
+        max_length=16, choices=RESOURCE_EFFECT_OPTIONS)
     
     class Meta:
         ordering = ('name',)
@@ -57,28 +136,64 @@ class Action(models.Model):
 
 @python_2_unicode_compatible
 class QuantityKind(models.Model):
-    # qudt:QuantityKind
+    """
+    qudt:QuantityKind:
+    http://www.qudt.org/pages/QUDToverviewPage.html
+    http://qudt.org/doc/2017/DOC_SCHEMA-QUDT-v2.0.html#Classes  
+    """
     name = models.CharField(_('name'), max_length=255)
+    
+    class Meta:
+        ordering = ('name',)
+        
+    def __str__(self):
+        return self.name 
     
     
 @python_2_unicode_compatible
 class Unit(models.Model):
-    # qudt:Unit
+    """
+    qudt:Unit:
+    http://qudt.org/doc/2017/DOC_VOCAB-UNITS-BASE.html
+    http://qudt.org/doc/2017/DOC_SCHEMA-QUDT-v2.0.html#Classes
+    """
     quantity_kind = models.ForeignKey(QuantityKind,
         verbose_name=_('quantity kind'), related_name="units")
     name = models.CharField(_('name'), max_length=255)
     
-          
+    class Meta:
+        ordering = ('name',)
+    
+    def __str__(self):
+        return self.name 
+
+        
 @python_2_unicode_compatible
 class QuantityValue(models.Model):
-    # qudt:QuantityValue
+    """
+    qudt:QuantityValue:
+    http://qudt.org/doc/2017/DOC_SCHEMA-QUDT-v2.0.html#Classes
+    """
     numeric_value = models.DecimalField(_('quantity'), max_digits=11, decimal_places=4) #todo: embiggen?
     unit = models.ForeignKey(Unit,
         verbose_name=_('unit'), related_name="quantity_values")
+        
+    class Meta:
+        ordering = ('unit',)
+        
+    def __str__(self):
+        return ' '.join([
+            str(self.numeric_value),
+            self.unit.name,
+        ])
     
     
 @python_2_unicode_compatible
 class EconomicEvent(models.Model):
+    """
+    vf:EconomicEvent:
+    https://valueflows.gitbooks.io/valueflows/content/specification/generated-spec.html#d4e747
+    """
     action = models.ForeignKey(Action,
         verbose_name=_('action'), related_name="events")
     provider = models.ForeignKey(Agent,
@@ -102,7 +217,18 @@ class EconomicEvent(models.Model):
         ordering = ('-date_and_time',)
 
     def __str__(self):
-        return self.name
+        
+        return ' '.join([
+            self.action.name,
+            self.date_and_time.strftime('%Y-%m-%d'),
+            'provider',
+            self.provider.name,
+            'receiver',
+            self.receiver.name,
+            self.quantity_value.__str__(),
+            self.resource.__str__(),
+        ])
+
         
         
 
