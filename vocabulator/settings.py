6472784b34c9f03669ae8dc03c0de8c9721254b7
Bob Haugen <bob.haugen@gmail.com>
2017-03-31 14:52:28 -0500

    change settings for deployment on pythonanywhere

--- vocabulator/settings.py
+++ vocabulator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6u=lu5%benyo=4g181l!qo0gsu_wp*bt^dj=l2ys9!$ol7g!6b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['valueflows.pythonanywhere.com']
 
 SITE_ID = 1
 
