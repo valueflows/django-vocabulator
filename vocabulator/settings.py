5b26cd1bfe60728e503a3a98e0e9afc6c8034d61
Bob Haugen <bob.haugen@gmail.com>
2017-03-31 15:03:37 -0500

    set STATIC_ROOT

--- vocabulator/settings.py
+++ vocabulator/settings.py
@@ -121,5 +121,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
